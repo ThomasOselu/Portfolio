dfe16a3ace00724d9b49bf246b149d052dbb6b05
ThomasOselu <117803665+ThomasOselu@users.noreply.github.com>
2024-01-26 01:55:12 +0300

    My portfolio website build using react framework
    and Javascript language, it displays by practice and expertise

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
+import { Projects } from "./components/Projects/Projects";
+import { Contact } from "./components/Contact/Contact";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:thomasoselu1@gmail.com">thomasoselu1@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/thomas-oselu-948b27241/">linkedin.com/Thomas Oselu</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/ThomasOselu">github.com/ThomasOselu</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
